9847a61cfc0843ffbe9e72e9296b96191fd1546c
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-08 16:45:31 -0700

    Parameterize network settings

--- example-infrastructure/WAN-vSwitch.tf
+++ example-infrastructure/WAN-vSwitch.tf
@@ -1,8 +1,8 @@
 # Declare the hyperv_network_switch resource
 # Get-NetAdapter
 resource "hyperv_network_switch" "wan" {
-  name                = "wan"
+  name                = var.wan_switch_name
   allow_management_os = true
   switch_type         = "External"
-  net_adapter_names   = ["Ethernet"]
+  net_adapter_names   = var.wan_adapter_names
 }
