8f285d74eb052f83bf6dd1daf37584a95bc780f1
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-09 18:13:57 -0700

    Fix Start-Process casing

--- runner_scripts/0114_Config-TrustedHosts.ps1
+++ runner_scripts/0114_Config-TrustedHosts.ps1
@@ -4,7 +4,7 @@ Invoke-LabStep -Config $Config -Body {
 
 if ($Config.SetTrustedHosts -eq $true) {
     
-    start-process cmd.exe -ArgumentList "/d /c winrm set winrm/config/client @{TrustedHosts=`"$Config.TrustedHosts`"}"
+    Start-Process cmd.exe -ArgumentList "/d /c winrm set winrm/config/client @{TrustedHosts=`"$Config.TrustedHosts`"}"
 
 } else {
     Write-CustomLog "SetTrustedHosts flag is disabled. Skipping TrustedHosts configuration."
