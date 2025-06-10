2cca9cf5502cd0efc99491dffe46630c7ae8d660
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-09 23:52:25 -0700

    refactor: move Get-ScriptAst to helper

--- tests/RunnerScripts.Tests.ps1
+++ tests/RunnerScripts.Tests.ps1
@@ -1,18 +1,11 @@
 . (Join-Path $PSScriptRoot 'TestDriveCleanup.ps1')
+. (Join-Path $PSScriptRoot 'helpers' 'Get-ScriptAst.ps1')
 $scriptDir = Join-Path $PSScriptRoot '..' 'runner_scripts'
 $scripts = Get-ChildItem $scriptDir -Filter '*.ps1'
 
 
 Describe 'Runner scripts parameter and command checks' -Skip:($IsLinux -or $IsMacOS) {
 
-function Get-ScriptAst {
-    param([string]$Path)
-    $text = Get-Content -Raw -Encoding UTF8 $Path
-    if ($text.Length -gt 0 -and $text[0] -eq [char]0xFEFF) {
-        $text = $text.Substring(1)
-    }
-    [System.Management.Automation.Language.Parser]::ParseInput($text, [ref]$null, [ref]$null)
-}
 
     $testCases = $scripts | ForEach-Object { @{ file = $_ } }
 
