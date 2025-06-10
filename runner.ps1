63f8a1936ec610ff71fbdb955d82de5c73d846e5
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-09 22:41:29 -0700

    Run scripts in new PowerShell processes

--- runner.ps1
+++ runner.ps1
@@ -183,11 +183,17 @@ function Invoke-Scripts {
                 }
             }
 
-            $cmd = Get-Command -Name $scriptPath -ErrorAction SilentlyContinue
-            $global:LASTEXITCODE = 0
+            $tempCfg = [System.IO.Path]::GetTempFileName()
+            $Config | ConvertTo-Json -Depth 5 | Out-File -FilePath $tempCfg -Encoding utf8
+            $sb = {
+                param($cfgPath, $scr)
+                $cfg = Get-Content -Raw -Path $cfgPath | ConvertFrom-Json
+                & $scr -Config $cfg
+                exit $LASTEXITCODE
+            }
 
-            if ($cmd -and $cmd.Parameters.ContainsKey('Config')) { . $scriptPath -Config $Config }
-            else                                               { . $scriptPath }
+            & pwsh -NoLogo -NoProfile -Command $sb -Args $tempCfg, $scriptPath
+            Remove-Item $tempCfg -ErrorAction SilentlyContinue
 
             $results[$s.Name] = $LASTEXITCODE
             if ($LASTEXITCODE) {
