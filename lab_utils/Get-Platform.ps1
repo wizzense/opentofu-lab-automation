cd99a737784cbade370be77a9fa27beb50704ccd
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-10 01:35:33 -0700

    Improve platform detection using RuntimeInformation

--- lab_utils/Get-Platform.ps1
+++ lab_utils/Get-Platform.ps1
@@ -1,7 +1,13 @@
 function Get-Platform {
-    if ($IsWindows) { return 'Windows' }
-    elseif ($IsLinux) { return 'Linux' }
-    elseif ($IsMacOS) { return 'MacOS' }
+    if ($IsWindows -or [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Windows)) {
+        return 'Windows'
+    }
+    elseif ($IsLinux -or [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Linux)) {
+        return 'Linux'
+    }
+    elseif ($IsMacOS -or [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::OSX)) {
+        return 'MacOS'
+    }
     elseif ($PSVersionTable.OS) {
         if ($PSVersionTable.OS -match 'Windows') { return 'Windows' }
         elseif ($PSVersionTable.OS -match 'Darwin') { return 'MacOS' }
