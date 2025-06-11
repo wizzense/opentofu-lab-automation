9b70dd0e4d7448e09834e8a3223462f86e766f6c
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-11 23:31:33 +0000

    pester 5.7.1

--- tests/helpers/TestHelpers.ps1
+++ tests/helpers/TestHelpers.ps1
@@ -5,8 +5,8 @@
 
 $SkipNonWindows = $IsLinux -or $IsMacOS
 
-# Ensure Pester v5.6.1 is loaded, not v3.x
-$desiredPesterVersion = '5.6.1'
+# Ensure Pester v5.7.1 is loaded, not v3.x
+$desiredPesterVersion = '5.7.1'
 $pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object { $_.Version -ge [version]$desiredPesterVersion } | Sort-Object Version -Descending | Select-Object -First 1
 if (-not $pesterModule) {
     Write-Error "Pester $desiredPesterVersion or newer is required. Run 'Install-Module -Name Pester -RequiredVersion $desiredPesterVersion -Force -Scope CurrentUser'."
