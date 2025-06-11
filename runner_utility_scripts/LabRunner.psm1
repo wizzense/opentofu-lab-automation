2f028873a7875d3c23a7aa38de63760ee0738bbc
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-10 21:53:04 -0700

    Fix LabRunner missing step functions

--- runner_utility_scripts/LabRunner.psm1
+++ runner_utility_scripts/LabRunner.psm1
@@ -2,4 +2,9 @@
 . $PSScriptRoot/Logger.ps1
 . $PSScriptRoot/../lab_utils/Get-Platform.ps1
 
+if (-not $script:LabRunner__Loaded) {
+    $script:LabRunner__Loaded = $true
+    . "$PSScriptRoot/ScriptTemplate.ps1"
+}
+
 Export-ModuleMember -Function Invoke-LabStep, Write-CustomLog, Get-Platform
