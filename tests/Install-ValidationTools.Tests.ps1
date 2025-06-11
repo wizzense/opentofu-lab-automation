a8bd59cb1c19a2bbe5ce046b64ef3582d8c0388b
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-11 09:09:26 -0700

    Load LabRunner module in validation tools tests

--- tests/Install-ValidationTools.Tests.ps1
+++ tests/Install-ValidationTools.Tests.ps1
@@ -2,6 +2,7 @@
 . (Join-Path $PSScriptRoot 'helpers' 'TestHelpers.ps1')
 Describe '0006_Install-ValidationTools'  {
     BeforeAll {
+        Import-Module (Join-Path $PSScriptRoot '..' 'lab_utils' 'LabRunner' 'LabRunner.psd1') -Force
         $script:ScriptPath = Get-RunnerScriptPath '0006_Install-ValidationTools.ps1'
         . (Join-Path $PSScriptRoot '..' 'lab_utils' 'LabRunner' 'Logger.ps1')
     }
