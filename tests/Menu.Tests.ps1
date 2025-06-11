72025c5db25f3c5f5da957d184c825a3fc2c075e
wizzense <37890504+wizzense@users.noreply.github.com>
2025-06-11 08:59:43 -0700

    Fix menu selection tests override scoping

--- tests/Menu.Tests.ps1
+++ tests/Menu.Tests.ps1
@@ -10,7 +10,7 @@ Describe 'Get-MenuSelection' {
     }
     It 'returns all items when user types all' {
         $items = @('0001_Test.ps1','0002_Other.ps1')
-        function global:Read-LoggedInput { param($Prompt) 'all' }
+        function Read-LoggedInput { param($Prompt) 'all' }
         $sel = Get-MenuSelection -Items $items -AllowAll
         $sel | Should -Be $items
     }
@@ -18,7 +18,7 @@ Describe 'Get-MenuSelection' {
         $items = @('0001_Test.ps1','0002_Other.ps1')
         $responses = @('0002')
         $script:i = 0
-        function global:Read-LoggedInput { param($Prompt) $responses[$script:i++] }
+        function Read-LoggedInput { param($Prompt) $responses[$script:i++] }
         $sel = Get-MenuSelection -Items $items
         $sel | Should -Be @('0002_Other.ps1')
     }
